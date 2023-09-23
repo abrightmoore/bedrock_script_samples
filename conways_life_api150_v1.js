a006483cbca57680ccbf87081b966c9667f055fb
Adrian Brightmoore <abrightmoore@yahoo.com.au>
2023-09-24 09:04:12 +1000

    A simple Game of Life implementation
    Performance improvements to reduce load on the game engine

--- conways_life_api150_v1.js
+++ conways_life_api150_v1.js
@@ -4,7 +4,7 @@ import * as mc from "@minecraft/server";
 //  Run a simulation of Conway's Game of Life in Minecraft Bedrock using blocks in the world
 //  Set up the working areas
 
-let field_size = 32;
+let field_size = 64;
 let field_frames = new Array(2);  // One current displayed frame, one working one. Double-buffered display.
 let iteration = 0;
 let current_frame = 0, next_frame = 1;
@@ -12,10 +12,15 @@ let num_neighbours = 0;
 let px, py, cell_state;  // Calculating next cell states
 let block, index, block_type;
 let field_origin = {x:0, y:225, z:0}; // TODO: position the field relative to a tagged entity for multiple simulations.
+let dimension = mc.world.getDimension("overworld");
 let cells = [
 				"minecraft:air",
 				"minecraft:glowstone"
 ];  // Dead,Alive. Avoid lighting updates
+
+for (let i = 0; i<cells.length; i++) {
+	cells[i] = mc.BlockPermutation.resolve(cells[i]);  // pre-calculate the block permutation
+};
 
 for (let j = 0; j<2; j++) {
 	field_frames[j] = new Array(field_size);  // Add # rows
@@ -24,12 +29,17 @@ for (let j = 0; j<2; j++) {
 		// Randomise the initial state
 		for (let k = 0; k < field_frames[j][i].length; k++) {
 			field_frames[j][i][k] = Math.floor(Math.random() * 2) // 0 empty or 1 alive. TODO: Allow pause and build
+			// Clear the play zone up front because we're not updating all the blocks every tick
+			if (j == 0) {  // Clear out blocks only once on the first loop
+				dimension.getBlock( {x: field_origin.x+k, y:field_origin.y+i, z:field_origin.z}).setPermutation(cells[0]);
+			}
 		}
 	}
 };
 
+
 mc.system.runInterval(() => {
-	let dimension = mc.world.getDimension("overworld");  // TODO: Get each tagged player, get its dimension, work with that
+  // TODO: Get each tagged player, get its dimension, work with that
 	
 	current_frame = iteration%2;  // 0 or 1
 	next_frame = (iteration+1)%2; // 1 or 0
@@ -38,7 +48,6 @@ mc.system.runInterval(() => {
 	// As we prepare the NEXT frame state in the memory buffer, we can push it out to the world to avoid 2xLoops
 	for (let y = 0; y < field_frames[current_frame].length; y++) {
 		for (let x = 0; x < field_frames[current_frame][y].length; x++) {
-			// field_frames[next_frame][y][x] = field_frames[current_frame][y][x];  // Clear ready for editing
 			cell_state = field_frames[current_frame][y][x];  // Copy for convenience 
 			num_neighbours = 0;
 			for (let dy = -1; dy < 2; dy++) {
@@ -62,11 +71,12 @@ mc.system.runInterval(() => {
 				field_frames[next_frame][y][x] = field_frames[current_frame][y][x]
 			}
 			// Use the value of the next frame cell to index the block type
-			block = dimension.getBlock( {x: field_origin.x+x, y:field_origin.y+y, z:field_origin.z});
 			// Now holding a handle on what is in space we can force the block into it
-			index = field_frames[next_frame][y][x]
-			block_type = cells[index]
-			block.setPermutation(mc.BlockPermutation.resolve(block_type));
+			// For performance improvements, only change blocks that have been changed
+			if (field_frames[next_frame][y][x] != cell_state) {
+				block = dimension.getBlock( {x: field_origin.x+x, y:field_origin.y+y, z:field_origin.z}).setPermutation(cells[field_frames[next_frame][y][x]]);
+			};
+
 		}
 	}
-},4)
+},2)
